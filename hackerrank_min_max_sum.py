c180ed6876f00cad92a6e547f2a106b42a08d6e1
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2025-02-17 23:14:54 +0530

    Add files via upload

---
+++ hackerrank_min_max_sum.py
@@ -0,0 +1,13 @@
+def min_max():
+    list1 = [1, 2, 3, 4, 5]
+    list2 = list()
+    min1 = 
+    for ele1  range(1 len(list1
+        print(" == ele1 -:",ele1 end=" == ")
+        for ele2 in range(ele1 len(list1)
+            print("ele2 -:",ele2,end=" 
+
+   printlist2 
+    print(list1index)
+
+min_max(
