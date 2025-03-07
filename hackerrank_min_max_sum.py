a1aae6ef0273f034e2694eca0a54f58d78cd5dd2
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2025-03-07 21:11:56 +0530

    Update hackerrank_min_max_sum.py

--- hackerrank_min_max_sum.py
+++ hackerrank_min_max_sum.py
@@ -2,8 +2,8 @@ def min_max():
     list1 = [1, 2, 3, 4, 5]
     list2 = list()
     min1 = 
-    for ele1  range(1 len(list1
-        print(" == ele1 -:",ele1 end=" == ")
+    for ele1 in range(1, len(list1)):
+        print(" == ele1 -:",ele1, end=" == ")
         for ele2 in range(ele1 len(list1)
             print("ele2 -:",ele2,end=" 
 
