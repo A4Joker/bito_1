1dab308987fea7cd76c80b7a500c90c1d6baf7a7
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2025-03-11 21:53:29 +0530

    Update hackerrank_min_max_sum.py

--- hackerrank_min_max_sum.py
+++ hackerrank_min_max_sum.py
@@ -7,7 +7,7 @@ def min_max():
         for ele2 in range(ele1 len(list1)
             print("ele2 -:",ele2,end=" ")
 
-   print(list2)
+   print(list
     print(list1)  # Print the list instead of undefined variable
 
-min_max()
+min_max
